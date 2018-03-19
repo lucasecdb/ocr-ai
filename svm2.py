4e83683a750194b5f9be2acafde1a43f0445a89f
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-03-18 21:18:05 -0300

    aumenta tamanho de treino pra 80%

--- svm2.py
+++ svm2.py
@@ -9,11 +9,8 @@ from sklearn import svm
 
 import numpy as np
 
-import scipy
-
 ###### Leitura dos dados de treino e respectivas labels ######
 
-hog_images = []
 data_labels = []
 hog_features = []
 
@@ -22,27 +19,22 @@ print('Efetuando a leitura dos dados...')
 for path, dir, files in os.walk('./data/normalized'):
     for file in files:
         label = re.search('0+(.+?)-', file)
-        # image = imread(os.path.join(path, file))
-        features = hog(image=imread(os.path.join(path, file)),orientations=8, block_norm='L2', pixels_per_cell=(4, 4), cells_per_block=(2,2))
-        # hog_images.append(image)
+        features = hog(image=imread(os.path.join(path, file)), orientations=8,
+                       block_norm='L2', pixels_per_cell=(4, 4), cells_per_block=(2, 2))
         hog_features.append(features)
         if label:
-        	data_labels.append(label.group(1))
+            data_labels.append(label.group(1))
 
-# hog_features = np.array(hog_features)
 data = np.array(hog_features)
-# data = data.reshape(len(data), -1)
 
-
-train_data, test_data, train_labels, test_labels = train_test_split(data, data_labels, test_size=0.6, random_state=0)
+train_data, test_data, train_labels, test_labels = train_test_split(
+    data, data_labels, test_size=0.2, random_state=0)
 
 print('DONE.\n')
 
 print('Treinando o classificador SVC...')
 
 svc = svm.SVC(C=32, gamma=0.0078125)
-# parameters = {'C': scipy.stats.expon(scale=100), 'gamma': scipy.stats.expon(scale=.1),'kernel': ['rbf', 'linear']}
-# agent = RandomizedSearchCV(svc, parameters)
 agent = svc.fit(train_data, train_labels)
 
 print('DONE.\n')
